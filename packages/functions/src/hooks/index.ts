8d83b7944646ef709b17d715e7240f9934dfe8dd
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2023-11-09 21:31:05 -0500

    Create useRef hook.

--- packages/functions/src/hooks/index.ts
+++ packages/functions/src/hooks/index.ts
@@ -2,5 +2,6 @@ export * from './use-callback';
 export * from './use-effect';
 export * from './use-layout-effect';
 export * from './use-memo';
+export * from './use-ref';
 export * from './use-state';
 export * from './utils';
